e4cc394654417c1aefbcd1b6dbdf17c806db8a79
Sergey Miroshnichenko <58503543+ommmirage@users.noreply.github.com>
2024-08-04 21:28:00 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata = {
     ? new URL(`https://${process.env.VERCEL_URL}`)
     : undefined,
   title: {
-    default: 'Next.js AI Chatbot',
+    default: 'Исток. Консультации с психологом онлайн',
     template: `%s - Next.js AI Chatbot`
   },
   description: 'An AI-powered chatbot template built with Next.js and Vercel.',
